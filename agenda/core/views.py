907a7ac6eba19978fa56ceea661e45e67b68aa08
Rennan Augusto Ates dos Santos <rennanaugusto@MacBook-Air-de-Rennan.local>
2023-04-23 18:02:30 -0300

    Criado a tela de listagem de eventos

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render, HttpResponse
 from .models import Evento
+
+
+
+
+
 # Create your views here.
 def eventos_titulo(request,titulo_evento):
     Eventos = Evento.objects.get(titulo = titulo_evento)
     local_evento = Eventos.usuario
     return HttpResponse(f'{local_evento}')
+
+def lista_eventos(request):
+    evento = Evento.objects.all
+    context = { 
+        'eventos': evento
+            
+            
+                }
+    return render(request, 'agenda.html', context)
+
+#def index(request):
+#    return redirect('/agenda/')
